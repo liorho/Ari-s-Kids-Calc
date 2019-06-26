353b982b7ce1b61e911e98fdd315973912939a5d
liorho <hoshealior@gmail.com>
2019-06-26 20:29:10 +0300

    Add Set Cursor

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
+// Declare Variables
 let num1, num2, operator, result;
 
-// Random number between 0-10
-const randomNum = function () {
-    return (Math.round(Math.random() * 10));
+// Unable key queue
+const enterKey = function(func, tag, keyNum){
+     document.getElementById(tag).addEventListener('keypress', function (event) {
+        if (event.keyCode == keyNum) {
+            func();
+        }
+    })
 }
 
-// Random IMG to display
-const randomImg = function () {
-    let num;
-    do { num = randomNum(); }
-    while (num === 10 || num === 0 || num===8 || num===9);
-    img = `img/${num}.png`;
-    return img;
-}
+// Restart the Game
+const restartGame = () => window.location.reload(true);
 
-// Inserting NUMBER element to the DOM
+// Random Number between 0-10
+const randomNum = () => Math.round(Math.random() * 10);
+
+// Display Number
 const insertNum = function (num, loc) {
     const location = document.getElementById(loc);
     const numElement = document.createElement("div");
@@ -22,7 +24,30 @@ const insertNum = function (num, loc) {
     location.appendChild(numElement);
 }
 
-// Random operator (+ or -) and RESULT
+// Random IMG
+const randomImg = function () {
+    let num;
+    do { num = randomNum(); }
+    while (num === 10 || num === 0 || num === 8 || num === 9);
+    img = `img/${num}.png`;
+    return img;
+}
+
+// Display IMGs
+const inserImg = function (num, loc) {
+    const location = document.getElementById(loc);
+    for (let i = 1; i <= num; i++) {
+        let img = randomImg();
+        let numImg = document.createElement("img");
+        numImg.src = img;
+        numImg.style.height = "33%";
+        numImg.style.width = "21%";
+        numImg.style.padding = "0.6vw";
+        location.appendChild(numImg);
+    }
+}
+
+// Random Operator (+ or -) & calculating Result
 const randomOperator = function () {
     let num = randomNum();
     if (num > 5) {
@@ -35,39 +60,38 @@ const randomOperator = function () {
     }
 }
 
-// Inserting the OPERATOR to the DOM
+// Display OPERATOR
 const setOperator = function (operator) {
     document.getElementById("operator").innerHTML = operator;
-}
-
-
-// Inserting the IMGs to the DOM
-const inserImg = function (num, loc) {
-    const location = document.getElementById(loc);
-    for (let i = 1; i <= num; i++) {
-        let img = randomImg();
-        let numImg = document.createElement("img");
-        numImg.src = img;
-        numImg.style.height = "23%";
-        numImg.style.width = "21%";
-        numImg.style.padding = "0.6vw";
-        location.appendChild(numImg);
-    }
 }
 
 // Displaying ERROR Massage
 const errorRespond = function (result) {
     const errorSign = document.createElement("div");
-    errorSign.style.height = "100%";
+    errorSign.style.minHeight = "100%";
+    errorSign.style.overflow = "auto";
+    errorSign.style.top = "0";
+    errorSign.style.left = "0";
+    errorSign.style.bottom = "0";
+    errorSign.style.right = "0";
+    errorSign.style.position = "fixed";
     errorSign.style.margin = "0";
-    errorSign.innerHTML = `<span style="font-size:7vw">WRONG ANSWER.</span><br> THE CORRECT ANSWER IS: <span style="font-size:11vw; color:red">` + result + "</span>.<br> TRY AGAIN";
+    errorSign.innerHTML = `<span style="font-size:8vw">WRONG ANSWER.</span>
+                        <br> THE CORRECT ANSWER IS: 
+                        <span style="font-size:11vw; color:red; font-family: 'Montserrat', sans-serif">`
+        + result + "</span>.<br> TRY AGAIN";
     errorSign.style.backgroundColor = "black";
     errorSign.style.color = "white";
     errorSign.style.textAlign = "center";
-    errorSign.style.fontSize = "5.5vw";
+    errorSign.style.fontSize = "6.1vw";
     errorSign.style.fontFamily = "'Gloria Hallelujah', cursive";
+
     document.body.innerHTML = '';
+
     document.body.appendChild(errorSign);
+
+    enterKey(restartGame, "body", 32);
+    document.getElementById("body").addEventListener("click", restartGame);
 }
 
 // Displaying CORRECT Massage
@@ -75,61 +99,78 @@ const correctRespond = function () {
     const correctSign = document.createElement("div");
     correctSign.style.height = "100";
     correctSign.style.margin = "0";
-    correctSign.innerHTML = `<span style="font-size:6vw">CORRECT ANSWER !!!</span><br>YOU ARE AMAZING, COME ON AND MAKE MY DAY AGAIN`;
+    correctSign.innerHTML = `<span style="font-size:6vw;">CORRECT ANSWER !!!</span><br>
+                            YOU ARE AMAZING. GO AHEAD, MAKE MY DAY (AGAIN)`;
     correctSign.style.color = "rgb(67,14,151)";
     correctSign.style.textAlign = "center";
     correctSign.style.fontSize = "4vw";
     correctSign.style.fontFamily = "'Gloria Hallelujah', cursive";
-    document.body.innerHTML = '';
-    document.body.appendChild(correctSign);
-    for (let i=0;i<6;i++){
-    let img = randomImg();
-    let numImg = document.createElement("img");
-    numImg.src = img;
-    numImg.style.height = "15%";
-    numImg.style.width = "15%";
-    numImg.style.padding = "0.6vw";
-    numImg.style.marginRight = "auto";
 
-    document.body.appendChild(numImg);
+    document.body.innerHTML = '';
+
+    document.body.appendChild(correctSign);
+    for (let i = 0; i < 6; i++) {
+        let img = randomImg();
+        let numImg = document.createElement("img");
+        numImg.src = img;
+        numImg.style.height = "15%";
+        numImg.style.width = "15%";
+        numImg.style.padding = "0.6vw";
+        numImg.style.marginRight = "auto";
+        document.body.appendChild(numImg);
     }
+
+    enterKey(restartGame, "body", 32);
+    document.getElementById("body").addEventListener("click", restartGame);
 }
 
 
-// CHecking if user RESULT is true and execute a RESPOND
+// Check if user RESULT is true and execute a RESPOND
 const checkResult = function () {
     let userResult = Number(document.getElementById("result_user").value);
     console.log(userResult)
     if (userResult === result) {
         correctRespond();
+        // Refresh();
     } else {
         errorRespond(result);
-
+        // Refresh();
     }
 }
 
-// Operating the PROGRAM
 
+// Set cursor 
+setCursor = () => $('input').focus();
+
+
+
+
+// ************ Operating the PROGRAM ******************
+
+// Pick 2 random Numbers
 num1 = randomNum();
 num2 = randomNum();
 
+// Display Numbers
 insertNum(num1, "num1_num");
 insertNum(num2, "num2_num");
 
+// Display Number's Images
 inserImg(num1, "num1_img");
 inserImg(num2, "num2_img");
 
+// Pick a random Operator
 randomOperator();
+
+// Display Operator
 setOperator(operator);
 
+// Set cursor 
+setCursor();
 
-console.log(result);
 
-// Unabling the ENTER key 
-document.getElementById('container').addEventListener('keypress', function (event) {
-    if (event.keyCode == 13) {
-        checkResult();
-    }
-})
+// Unable ENTER key to append result
+enterKey(checkResult, "container", 13);
+
 
 
